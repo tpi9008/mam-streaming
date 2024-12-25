d7b01c46aad629919df02cb4728fa98a9067d6d6
tpi9008 <walker.nathan@gmail.com>
2024-12-25 01:53:16 -0500

    Test S3 streaming with single file

--- src/services/OneDriveService.js
+++ src/services/OneDriveService.js
@@ -1,31 +1,23 @@
-export const fetchAudioFiles = async () => {
-    try {
-        // Convert share links to embed format
-        const files = [
-            {
-                id: '1',
-                name: 'mam010106.mp3',
-                audio: {
-                    title: 'mam010106',
-                    // Convert from share to embed URL
-                    streamUrl: 'https://onedrive.live.com/embed?cid=A0E6F611C5599F2C&resid=A0E6F611C5599F2C%21620692&authkey=AFIjS7lMsmCkQdo'
-                }
-            }
-        ];
-        return files;
-    } catch (error) {
-        console.error("Error in fetchAudioFiles:", error);
-        throw error;
+// Static list of episode files
+const S3_BASE_URL = 'https://mamstreaming.s3.us-east-2.amazonaws.com/';
+
+const audioFiles = [
+    {
+        id: '1',
+        name: 'mam010106.mp3',
+        audio: {
+            title: 'mam010106',
+            streamUrl: S3_BASE_URL + 'mam010106.mp3'
+        }
     }
+    // We'll add all files here after upload
+];
+
+export const fetchAudioFiles = async () => {
+    return audioFiles;
 };
 
 export const getAudioStreamUrl = async (fileId) => {
-    try {
-        const files = await fetchAudioFiles();
-        const match = files.find(f => f.id === fileId);
-        return match?.audio?.streamUrl;
-    } catch (error) {
-        console.error("Error in getAudioStreamUrl:", error);
-        throw error;
-    }
+    const file = audioFiles.find(f => f.id === fileId);
+    return file?.audio?.streamUrl;
 };
